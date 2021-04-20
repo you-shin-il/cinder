b25681b4cfa6d289296e0bc70f10a09fe58c26a3
ysi <kyoyaiida@injeinc.co.kr>
2021-04-20 17:59:07 +0900

    수정

--- src/main/java/com/ysi/openstack/cinder/web/CinderController.java
+++ src/main/java/com/ysi/openstack/cinder/web/CinderController.java
@@ -1,20 +1,16 @@
 package com.ysi.openstack.cinder.web;
 
-import com.ysi.openstack.cinder.model.volume.volume.Volume;
 import com.ysi.openstack.cinder.model.volume.volume.VolumeRequestVO;
 import com.ysi.openstack.cinder.model.volume.volume.create.CreateVolumeDTO;
 import com.ysi.openstack.cinder.service.volume.VolumeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URISyntaxException;
 
-@Controller
+@RestController
 public class CinderController {
     @Autowired
     private VolumeService volumeService;
@@ -28,14 +24,13 @@ public class CinderController {
     }*/
 
     @GetMapping("/cinder/volumes")
-    @ResponseBody
-    public Volume getCinderVolumes(@RequestBody(required = false) VolumeRequestVO volumeRequestVO) throws URISyntaxException {
-        ResponseEntity<Volume> responseEntity = volumeService.volumes(volumeRequestVO);
-        return responseEntity.getBody();
+    public ResponseEntity<?> getCinderVolumes(@RequestBody(required = false) VolumeRequestVO volumeRequestVO) throws URISyntaxException {
+	    return volumeService.volumes(volumeRequestVO);
+/*        ResponseEntity<Volume> responseEntity = volumeService.volumes(volumeRequestVO);
+        return responseEntity.getBody();*/
     }
 
     @PostMapping("/cinder/create/volumes")
-    @ResponseBody
     public void createCinderVolume(@RequestBody CreateVolumeDTO createVolumeDTO) {
         System.out.println("createVolumeDTO = " + createVolumeDTO);
     }
